7e241d24bd18e6b5eca2dc2adb00cf68fb10cd90
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-03-04 08:55:25 +0300

    refactor: modify setup function

--- src/components/sidebar/__test__/LocationInfo.test.js
+++ src/components/sidebar/__test__/LocationInfo.test.js
@@ -7,8 +7,14 @@ import { userEvent } from "@testing-library/user-event";
 import LocationInfo from "../LocationInfo.vue";
 import { useBookmarkStore } from "@/stores/bookmark";
 
-function setupWithEmptyBookmarks() {
+function setup(bookmarks = []) {
   const mockRefetch = vi.fn();
+  const user = userEvent.setup();
+  const pinia = createTestingPinia();
+  const bookmarkStore = useBookmarkStore(pinia);
+
+  bookmarkStore.bookmarks = bookmarks;
+  bookmarkStore.$cookies = { get: vi.fn(), set: vi.fn() };
 
   render(LocationInfo, {
     props: {
@@ -19,16 +25,14 @@ function setupWithEmptyBookmarks() {
       refetch: mockRefetch,
     },
     global: {
-      plugins: [createTestingPinia()],
+      plugins: [pinia],
     },
   });
-  const bookmarkStore = useBookmarkStore();
-  bookmarkStore.$cookies = { get: vi.fn(), set: vi.fn() };
 
   const locationName = screen.getByRole("heading", { name: /london/i });
   const localTime = screen.getByText(/local time/i);
   const refreshButton = screen.getByRole("button", { name: /refresh/i });
-  const addBookmarkButton = screen.getByRole("button", { name: /add/i });
+  const addBookmarkButton = screen.queryByRole("button", { name: /add/i });
   const removeBookmarkButton = screen.queryByRole("button", { name: /remove/i });
 
   return {
@@ -39,31 +43,8 @@ function setupWithEmptyBookmarks() {
     removeBookmarkButton,
     bookmarkStore,
     mockRefetch,
+    user,
   };
-}
-
-function setupWithBookmarks() {
-  const pinia = createTestingPinia();
-  const bookmarkStore = useBookmarkStore(pinia);
-  bookmarkStore.bookmarks = ["London"];
-
-  render(LocationInfo, {
-    props: {
-      location: {
-        name: "London",
-        localtime: "2024-03-03 12:00",
-      },
-      refetch: vi.fn(),
-    },
-    global: {
-      plugins: [pinia],
-    },
-  });
-
-  const removeBookmarkButton = screen.queryByTitle("Remove Bookmark");
-  const addBookmarkButton = screen.queryByTitle("Add to Bookmarks");
-
-  return { removeBookmarkButton, addBookmarkButton, bookmarkStore };
 }
 
 describe("LocationInfo", () => {
@@ -74,7 +55,7 @@ describe("LocationInfo", () => {
       refreshButton,
       addBookmarkButton,
       removeBookmarkButton,
-    } = setupWithEmptyBookmarks();
+    } = setup();
 
     expect(locationName).toBeInTheDocument();
     expect(localTime).toBeInTheDocument();
@@ -82,9 +63,9 @@ describe("LocationInfo", () => {
     expect(addBookmarkButton).toBeInTheDocument();
     expect(removeBookmarkButton).not.toBeInTheDocument();
   });
+
   it("should refresh when refresh button is clicked", async () => {
-    const { refreshButton, mockRefetch } = setupWithEmptyBookmarks();
-    const user = userEvent.setup();
+    const { refreshButton, mockRefetch, user } = setup();
 
     await user.click(refreshButton);
     expect(mockRefetch).toHaveBeenCalled();
@@ -92,15 +73,14 @@ describe("LocationInfo", () => {
 
   describe("when location is not bookmarked", () => {
     it("should render add to bookmark button", () => {
-      const { addBookmarkButton, removeBookmarkButton } =
-        setupWithEmptyBookmarks();
+      const { addBookmarkButton, removeBookmarkButton } = setup();
 
       expect(addBookmarkButton).toBeInTheDocument();
       expect(removeBookmarkButton).not.toBeInTheDocument();
     });
+
     it("should add location to bookmark when button is clicked", async () => {
-      const { addBookmarkButton, bookmarkStore } = setupWithEmptyBookmarks();
-      const user = userEvent.setup();
+      const { addBookmarkButton, bookmarkStore, user } = setup();
 
       await user.click(addBookmarkButton);
       expect(bookmarkStore.addBookmark).toBeCalledWith("London");
@@ -109,14 +89,14 @@ describe("LocationInfo", () => {
 
   describe("when location is bookmarked", () => {
     it("should render remove bookmark button", async () => {
-      const { addBookmarkButton, removeBookmarkButton } = setupWithBookmarks();
+      const { addBookmarkButton, removeBookmarkButton } = setup(["London"]);
 
       expect(addBookmarkButton).not.toBeInTheDocument();
       expect(removeBookmarkButton).toBeInTheDocument();
     });
+
     it("should remove location from bookmark when button is clicked", async () => {
-      const { removeBookmarkButton, bookmarkStore } = setupWithBookmarks();
-      const user = userEvent.setup();
+      const { removeBookmarkButton, bookmarkStore, user } = setup(["London"]);
 
       await user.click(removeBookmarkButton);
       expect(bookmarkStore.removeBookmark).toBeCalledWith("London");
