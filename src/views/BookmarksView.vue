9b8332f293cb9721c017fe92a8259149731371fb
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-03-09 14:48:06 +0300

    feat: add icon for no bookmark message to bookmarks view

--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -6,14 +6,15 @@ const bookmarkStore = useBookmarkStore();
 </script>
 
 <template>
-  <div v-if="bookmarkStore.bookmarks?.length === 0" class="message">
-    <p>
-        You have no saved bookmarks yet. 
-    </p>
-    <p>
-        You can add that location as a bookmark by
-        pressing the bookmark icon next to the location name.
-    </p>
+  <div v-if="bookmarkStore.bookmarks?.length === 0" class="info">
+    <v-icon scale="10" name="bi-journal-bookmark-fill" />
+    <div class="message">
+      <p class="message-title">You have no saved bookmarks yet.</p>
+      <p>
+        You can add that location as a bookmark by pressing the bookmark icon
+        next to the location name.
+      </p>
+    </div>
   </div>
   <div class="bookmarks" v-else>
     <div v-for="bookmark in bookmarkStore.bookmarks" :key="bookmark">
@@ -23,9 +24,26 @@ const bookmarkStore = useBookmarkStore();
 </template>
 
 <style scoped>
+.info {
+  display: flex;
+  flex-direction: column;
+  height: 90%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
+
 .message {
   margin-top: 2rem;
   font-size: 1.2rem;
+  text-align: center;
+  max-width: 600px;
+}
+
+.message-title {
+  font-size: 1.5rem !important;
+  font-weight: 500;
+  margin-bottom: 0.5rem;
 }
 
 .bookmarks {
@@ -36,6 +54,12 @@ const bookmarkStore = useBookmarkStore();
   overflow-x: hidden;
   height: calc(900px - 4rem - 45px);
   padding-right: 0.5rem;
+}
+
+@media screen and (max-width: 1440px) {
+  .info {
+    padding-top: 2rem;
+  }
 }
 
 @media (max-width: 768px) {
